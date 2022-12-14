56ca53cebe36fa833c0768f2d8ef91685b91cd83
vk <v.krinitsyn@gmail.com>
2022-12-14 15:30:40 -0600

    removed table_coid from BoundaryType ASCII check in ytsetup no trigger create on ytsetup azure build only on master

--- docker.sh
+++ docker.sh
@@ -7,3 +7,7 @@ echo "Docker build completed, starting the init.sh..."
 echo
 
 docker run -it -v $(pwd):/home/rust yaxaha bash -e init.sh
+postgres=# \d public.yt_config⁩
+Did not find any relation named "public.yt_config⁩".
+postgres=# \d public.yt_config
+
