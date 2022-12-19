31abfa544ee829a48c59d9edfcd20bce2bc3a612
Natalia Krinitsyn <nv@dbinvent.com>
2022-12-19 08:49:31 -0600

    fixed table type to A

--- it_prepare_test.sql
+++ it_prepare_test.sql
@@ -8,11 +8,11 @@ DROP TRIGGER IF EXISTS yt_config_test_trigger ON yt_config;
 
 delete from yt_config where name like '%yt_config';
 -- see print_table_cfg_v1
-insert into yt_config(name, module, value) values ('.'|| current_database() ||'.public.yt_config', 'D', '');
+insert into yt_config(name, module, value) values ('.'|| current_database() ||'.public.yt_config', 'A', '');
 
 \echo prepare TRIGGER on yt_config
 
-CREATE TRIGGER yt_config_test_trigger AFTER INSERT OR UPDATE OR DELETE ON yt_config FOR EACH ROW EXECUTE PROCEDURE yt_synchronize();
+-- CREATE TRIGGER yt_config_test_trigger AFTER INSERT OR UPDATE OR DELETE ON yt_config FOR EACH ROW EXECUTE PROCEDURE yt_synchronize();
 
 \echo "call yt_info with e and i"
 select name, module, value from yt_config where module <> ' '
