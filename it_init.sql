a71e6a3687060cb3e96721fd8c67c2e9e823da1a
Natalia Krinitsyn <nv@dbinvent.com>
2026-03-01 17:03:36 -0600

    init

--- it_init.sql
+++ it_init.sql
@@ -13,7 +13,7 @@ insert into public.yt_config (name, module, value) values
  ('node2','N','127.0.0.1:28772'), -- -6e6f6465326e
  ('node3','N','127.0.0.1:28773'), -- -6e6f6465336e
  ('ping_tick_sleep_ms','C','5000'), -- delay between pings
- ('log_level','C','debug'),
+--  ('log_level','C','debug'),
  ('fs_dir','C','./cluster'),
  ('cfg_level','C','5'), -- use: 'node uuid . IP . port' as logs prefix
  ('verbose','C','true'), -- extra logs
--- it_test.sql
+++ it_test.sql
@@ -1,7 +1,9 @@
 -- select * from json_to_recordset(yt_info('EI')) as x(name text, module text, value text);
 \echo before
+select * from yt_config where module = ' ';
+select 'The leader node is: ' || value from json_to_recordset(yt_info('E')) as x(name text, value text) where name = 'leader';
 
-select * from json_to_recordset(yt_info('i')) as x(name text, module text, value text);
+-- select * from json_to_recordset(yt_info('i')) as x(name text, module text, value text);
 begin;
 
 insert into yt_config(name, value) values ('test1', 'test');
@@ -9,7 +11,12 @@ insert into yt_config(name, value) values ('test1', 'test');
 select yt_complete(true);
 commit;
 
-select pg_sleep(1);
+-- select pg_sleep(1);
 
-select * from json_to_recordset(yt_info('i')) as x(name text, module text, value text);
+-- select * from json_to_recordset(yt_info('i')) as x(name text, module text, value text);
 \echo after
+
+-- select * from json_to_recordset(yt_info('i')) as x(name text, module text, value text);
+-- select value from yt_info('d') where name = 'leader'
+
+select * from yt_config where module = ' ';
